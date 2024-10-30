34aa8a467ba54633336a96a161c01090d45e413a
stenlyadam <stenly.adam@unklab.ac.id>
2024-10-30 09:37:53 +0800

    update props

--- app-fufufafa/src/App.js
+++ app-fufufafa/src/App.js
@@ -1,17 +1,15 @@
-import Header from './components/Header'
-import Home from './components/Home'
-import Footer from './components/Footer'
-
-
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 const App = () => {
-  const JeremyApp= "Hello (App)"
+  const JeremyApp = "Hello (App)";
   return (
     <div>
       <h1>{JeremyApp}</h1>
-      <Header/>
-      <Home/>
-      <Footer/>
+      <Header text="Header" />
+      <Home />
+      <Footer />
     </div>
   );
 };

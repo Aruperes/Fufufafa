f064f2fcc2fa629853d74d22c9a47b1af6cee69c
Emily <s22210189@student.unklsb.ac.id>
2024-11-02 01:58:08 -0700

    mily add props

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,12 @@
 import Search from "./Search";
 import CardInfo from "./Card-Info";
-const Home = ({text}) => {
+const Home = ({ text }) => {
   const JeremyHome = "Hello (Home)";
   return (
     <div>
       <h2>{text}</h2>
-      <Search />
-      <CardInfo />
+      <CardInfo text="cardinfoProps" />
+      <Search text="searchProps" />
     </div>
   );
 };
